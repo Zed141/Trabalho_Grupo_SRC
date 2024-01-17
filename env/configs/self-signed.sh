c88929d297eab5e4aa2ba5cab816ab5370c743b9
Knitter <knitter.is@gmail.com>
2024-01-17 18:40:30 +0000

    TASK Atualiza configurações NGINX e regista commandos para criar certificado auto-assinado

---
+++ env/configs/self-signed.sh
@@ -0,0 +1,10 @@
+# Em shell; Nota: IP a usar no certificado é o IP da VM (192.168.86.100 atualmente)
+# Não faz parte do script de configuração da máquina por obrigar a preenchimento manual de informação
+# Executar os comandos como "root" ou colocar "sudo" antes de cada um
+#
+# Criar o certificado e o par de chaves necessário para o assinar; indicar que não é um "certificate request" mas sim a geração final
+#
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+# Criar um valor para forte para processo de FS (Forward Secrecy, https://en.wikipedia.org/wiki/Forward_secrecy) usado pelo Diffie-Hellman do HTTPS (TLS)
+#
+dhparam -out /etc/ssl/certs/dhparam.pem 2048
