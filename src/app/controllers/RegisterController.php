a8c86fcf81c976aaca37d3cce914279e7d4640e9
Knitter <knitter.is@gmail.com>
2024-01-17 00:01:51 +0000

    JS/PHP Example

--- src/app/controllers/RegisterController.php
+++ src/app/controllers/RegisterController.php
@@ -7,6 +7,23 @@ use yii\web\Controller;
 final class RegisterController extends Controller {
 
     public function actionIndex(): string {
+
+        //REGISTO!
+        //nome do user, e-mail do user, pública
+        //
+        // Gerar o para RSA em JS
+        //
+        //controlador: verificar se o e-mail existe
+        //SE NOK -> ERRO
+        //SE OK -> $user->new User();
+        //  preencher os atributos; $user->name = ...
+        //                          $user->email = ...
+        //  $user->save();
+        //
+        //
+
+
+
         return $this->render('index', ['name' => date('Y-m-d H:i:s')]);
     }
 }
--- src/public/static/js/register.js
+++ src/public/static/js/register.js
@@ -1,4 +1,6 @@
 (() => {
     let name = document.getElementById('shared-param').value;
     alert('hello world! -> ' + name);
+
+
 })();
