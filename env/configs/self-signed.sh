8b061d39b75ded8d1e8d18c3daa53a3f10e1cf1d
José Irio <jose.diogo141@gmail.com>
2024-01-18 00:05:06 +0000

    par de chaves PEM e guardar em Indexed DB

--- env/configs/self-signed.sh
+++ env/configs/self-signed.sh
@@ -4,7 +4,7 @@
 #
 # Criar o certificado e o par de chaves necessário para o assinar; indicar que não é um "certificate request" mas sim a geração final
 #
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+  openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
 # Criar um valor para forte para processo de FS (Forward Secrecy, https://en.wikipedia.org/wiki/Forward_secrecy) usado pelo Diffie-Hellman do HTTPS (TLS)
 #
-dhparam -out /etc/ssl/certs/dhparam.pem 2048
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
